fe6a376310f503f9bce65727783155d767f4b947
Tuhin Ghose <tuhin.ghose@mail.utoronto.ca>
2023-09-18 19:11:56 -0400

    Finished implementation of Activity 3

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
