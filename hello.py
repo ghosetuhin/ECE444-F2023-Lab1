bcf807778e72913e6a7b4b8a300aac8b19f4591b
Tuhin Ghose <tuhin.ghose@mail.utoronto.ca>
2023-09-18 21:26:59 -0400

    Added uoft email box implementation for activity 4

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisIsAHardToGuessString321123'
@@ -13,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,11 +22,18 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        form.name.data = None
+        session['email_warning'] = 'utoronto' not in form.email.data
+        session['email'] = form.email.data
+        form.email.data = None
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), email_warning=session.get('email_warning'))
 
 @app.route('/user/<name>')
 def user(name):
